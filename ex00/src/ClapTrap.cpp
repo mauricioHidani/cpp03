7bc2e81f25763638ad22267a7b63e620de174671
Mauricio Mityo Hidani <mauricio.hidani.work@gmail.com>
2026-03-15 22:05:18 -0300

    test: ex00 claptrap

--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: mhidani <mhidani@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/15 19:50:17 by mhidani           #+#    #+#             */
-/*   Updated: 2026/03/15 20:01:20 by mhidani          ###   ########.fr       */
+/*   Updated: 2026/03/15 22:04:59 by mhidani          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,20 @@
 
 ClapTrap::ClapTrap(std::string name):
 	_name(name), _hitPoints(10), _energyPoints(10), _attackDamage(0) {
+		std::cout << getName() << " is alive" << std::endl;
 }
 
 ClapTrap::ClapTrap(const ClapTrap &other):
 	_name(other.getName()), _hitPoints(other.getHitPoints()), 
 	_energyPoints(other.getEnergyPoints()), _attackDamage(other.getAttackDamage()) {
+	std::cout << getName() 
+			  << " is alive and has the same characteristics as " 
+			  << other.getName() 
+			  << std::endl;
 }
 
 ClapTrap::~ClapTrap(void) {
+	std::cout << getName() << " is dead" << std::endl;
 }
 
 std::string	ClapTrap::getName(void) const {
@@ -38,6 +44,36 @@ int	ClapTrap::getEnergyPoints(void) const {
 
 int ClapTrap::getAttackDamage(void) const {
 	return _attackDamage;
+}
+
+void	ClapTrap::attack(const std::string &target) {
+	std::cout << "ClapTrap "
+			  << getName() 
+			  << " attacks "
+			  << target
+			  << ", causing "
+			  << getAttackDamage()
+			  << " points of damage!"
+			  << std::endl;
+}
+
+void	ClapTrap::takeDamage(unsigned int amount) {
+	if (amount < 0)
+	{
+		std::cout << "It cannot cause harm" << std::endl;
+		return ;
+	}
+	_hitPoints -= amount;
+	_attackDamage = amount;
+	std::cout << std::endl;
+}
+
+void	ClapTrap::beRepaired(unsigned int amount) {
+	if (amount <= 0) {
+		return ;
+	}
+	_hitPoints += amount;
+	_attackDamage = 0;
 }
 
 ClapTrap	&ClapTrap::operator=(const ClapTrap &other) {
